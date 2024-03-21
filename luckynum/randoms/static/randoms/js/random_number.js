57b4ab645bac1080fc054b52d96fb86492049717
nicer <antonchernysh146@gmail.com>
2024-03-21 21:30:17 +0300

    change script of generation random number

--- luckynum/randoms/static/randoms/js/random_number.js
+++ luckynum/randoms/static/randoms/js/random_number.js
@@ -3,23 +3,21 @@ var number = document.getElementById("number");
 var numberBlock = document.querySelector(".block-random-number");
 
 genNumber.addEventListener('click', function() {
-    let fromNumber = document.getElementById('from').value;
-    let toNumber = document.getElementById('to').value;
-    let randomNumber = random(fromNumber, toNumber);
+    let fromNumber = parseInt(document.getElementById('from').value);
+    let toNumber = parseInt(document.getElementById('to').value);
+    const randomNumberGen = random(fromNumber, toNumber);
 
-
-    let countNumbers = `${randomNumber}`.length; 
-    let strRandomNumber = `${randomNumber}`;
+    let countNumbers = `${randomNumberGen}`.length; 
+    let strRandomNumber = `${randomNumberGen}`;
     numberBlock.textContent = '';
     for (let i = 0; i < countNumbers; i++) {
-        let start = Date.now(); // запомнить время начала
+        let start = Date.now();
 
         let timer = setInterval(function() {
-        // сколько времени прошло с начала анимации?
             let timePassed = Date.now() - start;
 
             if (timePassed >= 100) {
-                clearInterval(timer); // закончить анимацию через 2 секунды
+                clearInterval(timer);
                 return;
             }
   
@@ -40,9 +38,9 @@ genNumber.addEventListener('click', function() {
 
         numberBlock.append(newElement);
     }
-    // number.textContent = randomNumber; // Генерация числа
 });
 
+// генерация случайного числа
 function random(from=0, to=10) {
     const randomNumber = Math.random();
     let min = null;
@@ -61,10 +59,9 @@ function random(from=0, to=10) {
         return number;
     } else {
         let randomNumber2 = Math.random();
-
         for (var number2 = parseInt(`${randomNumber2}`.substring(2, `${max}`.length+2)); (number2 >= min && number2 <= max) === false;) {
             randomNumber2 = Math.random();
-            number2 = parseInt(`${randomNumber2}`.substring(2, `${max}`.length+2))
+            number2 = parseInt(`${randomNumber2}`.substring(2, `${max}`.length+2));
         }
 
         return number2;
